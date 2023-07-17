cf4ed879240d6340738ea7db15101f68ee125172
cloudhuh <claudiu.huhulia@gmail.com>
2023-07-17 17:01:48 +0200

    snack 2

--- snack2/result.php
+++ snack2/result.php
@@ -3,4 +3,13 @@ $name = $_GET['name'];
 $email = $_GET['email'];
 $age = $_GET['age'];
 
-var_dump($name, $email, $age);
+if (strlen($name) > 3 && str_contains($email, '.') && str_contains($email, '@') && is_numeric($age)) {
+
+    echo ('Accesso riuscito');
+} elseif (strlen($name) < 3) {
+    echo ('Il campo nome deve contenere almeno 3 caratteri');
+} elseif (!str_contains($email, '.') or !str_contains($email, '@')) {
+    echo ('Il campo email deve contenere almeno una chiocciola e un punto');
+} elseif (!is_numeric($age)) {
+    echo ('il campo età deve essere un numero');
+}
